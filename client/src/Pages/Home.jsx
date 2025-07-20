4f9bbdcd68a26e73b1752f92b16fbc9af2f678bd
harsh sharma <harshsunami75@gmail.com>
2025-07-20 18:06:21 +0530

    again a step of resolve of issue of renaming

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import Servicesection from '../Components/Servicesection';
 import Aboutsection from '../Components/Aboutsection';
-import Contactform from '../Components/contactform';
+import ContactForm from '../Components/ContactForm';
 
 function Home() {
   const subheadings = [
